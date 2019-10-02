8cf641b49c462f62f3dc3e94dacf56d8f801ddcc
Chris <chris.wittyname@gmail.com>
2019-10-02 22:34:08 +1300

    Updated main in package.json. Created index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// components
+export { default as Button } from './components/Button/Button';
+
+// themes
+export { default as DefaultTheme } from './themes/default';
+export { default as DefaultDarkTheme } from './themes/default-dark';
